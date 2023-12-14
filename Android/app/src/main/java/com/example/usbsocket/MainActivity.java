f70e74afe6516938f2b262bfc33daff8c77f780f
Junxiao Liao <100368763+shiiiiikiiiii@users.noreply.github.com>
2023-12-14 15:27:24 +0800

    Refactor code
    Refactor code, it is now more convenient to call methods

--- Android/app/src/main/java/com/example/usbsocket/MainActivity.java
+++ Android/app/src/main/java/com/example/usbsocket/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText inputEdit;
     private TextView receiveText;
-
     private UsbSocketUtils socketUtils;
 
     @Override
@@ -24,63 +24,25 @@ public class MainActivity extends AppCompatActivity {
 
         socketUtils = new UsbSocketUtils(new UsbSocketUtils.OnReceiverListener() {
             @Override
-            public void onReceived(final String text) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // 在主线程中更新UI的操作
-                        receiveText.append(text + "\n");
-                    }
-                });
+            public void onReceived(String text) {
+                receiveText.append(text + "\n");
             }
         });
 
-        // 接收
-        new Thread() {
+        socketUtils.connect();
 
-            @Override
-            public void run() {
-
-                while(true) {
-                    try {
-                        socketUtils.connect();
-                        if(socketUtils.isConnected()) {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    socketUtils.startReceiver();
-                                }
-                            });
-                            break;
-                        }
-                    } catch (Exception e) {
-                        // 连接异常
-                    }
-
-                    // 连接失败,1秒后重试
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-
-        }.start();
-
-        // 发送
         inputEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        socketUtils.sendText(inputEdit.getText().toString());
-                    }
-                }).start();
+                socketUtils.send(inputEdit.getText().toString());
                 return false;
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        socketUtils.disconnect();
     }
 }
--- Android/app/src/main/java/com/example/usbsocket/UsbSocketUtils.java
+++ Android/app/src/main/java/com/example/usbsocket/UsbSocketUtils.java
@@ -13,15 +13,12 @@ public class UsbSocketUtils {
 
     private static final String TAG = "UsbSocket";
 
-    // Server socket to listen for USB device connections
-    private ServerSocket serverSocket;
-    // Client socket once USB device is connected
     private Socket clientSocket;
-    // Streams for sending and receiving data
     private DataInputStream inputStream;
     private DataOutputStream outputStream;
 
-    // Interface for receiving data callbacks
+    private boolean isRunning;
+
     public interface OnReceiverListener {
         void onReceived(String text);
     }
@@ -32,66 +29,69 @@ public class UsbSocketUtils {
         this.receiverListener = listener;
     }
 
-    // Accept USB connection and open streams
     public boolean connect() {
-        try {
-            // Create server socket to listen for connections
-            serverSocket = new ServerSocket(9000);
-            // Accept connection from USB client
-            clientSocket = serverSocket.accept();
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(9000);
+                clientSocket = serverSocket.accept();
 
-            // Get input and output streams
-            inputStream = new DataInputStream(clientSocket.getInputStream());
-            outputStream = new DataOutputStream(clientSocket.getOutputStream());
-            return true;
-        } catch (IOException e) {
-            Log.e(TAG, "connect: ", e);
-        }
-        return false;
+                inputStream = new DataInputStream(clientSocket.getInputStream());
+                outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+                isRunning = true;
+                startReceiverThread();
+            } catch (IOException e) {
+                Log.e(TAG, "connect: ", e);
+            }
+        }).start();
+
+        return true;
     }
 
-    public boolean isConnected() {
-        return clientSocket != null && !clientSocket.isClosed();
-    }
-
-    // Start thread to listen for incoming data
-    public void startReceiver() {
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-
-                // Continuously read input stream
-                while(true) {
-                    try {
-                        byte[] buffer = new byte[256];
-                        int len = inputStream.read(buffer);
-                        if(len > 0) {
-                            // Read data if available
-                            String text = new String(buffer, 0, len);
-                            // Call receiver callback
-                            receiverListener.onReceived(text);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
+    private void startReceiverThread() {
+        new Thread(() -> {
+            while(isRunning) {
+                try {
+                    byte[] buffer = new byte[256];
+                    int len = inputStream.read(buffer);
+                    if(len > 0) {
+                        String text = new String(buffer, 0, len);
+                        receiverListener.onReceived(text);
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-
         }).start();
     }
 
-    // Send text data to the USB device
-    public boolean sendText(String text) {
-        try {
-            outputStream.writeUTF(text);
-            outputStream.flush();
-            return true;
-        } catch (IOException e) {
-            Log.e(TAG, "sendText: ", e);
-        }
-        return false;
+    public void disconnect() {
+        new Thread(() -> {
+            isRunning = false;
+            closeSocket();
+        }).start();
     }
 
+    private void closeSocket() {
+        try {
+            if(clientSocket != null) {
+                inputStream.close();
+                outputStream.close();
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "closeSocket: ", e);
+        }
+    }
+
+    public void send(String text) {
+        new Thread(() -> {
+            try {
+                outputStream.writeUTF(text);
+                outputStream.flush();
+            } catch (IOException e) {
+                Log.e(TAG, "sendText: ", e);
+            }
+        }).start();
+    }
 }
